763ff4dbbbd4f902548ed38c8adbb03b66b62f56
kiril212003@gmail.com <kiril212003@gmail.com>
2022-10-05 22:50:59 +0300

    Add project files.

---
+++ ConsoleApp/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Converter
+    {
+        private decimal UsdUah, EurUah;
+        public Converter(decimal u, decimal e)
+        {
+            UsdUah = u; EurUah = e;
+        }
+        public decimal UahToUsd(decimal h) => h / UsdUah;
+        public decimal UahToEur(decimal h) => h / EurUah;
+        public decimal UsdToUah(decimal u) => u * UsdUah;
+        public decimal EurToUah(decimal e) => e * EurUah;
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp;
+internal class Program
+{ 
+    private static void Main(string[] args)
+    {
+        Converter Ex = new(37.1m,36.8m);
+        Console.WriteLine("Input currency you have(UAH, EUR, USD):");
+        switch (Console.ReadLine())
+        {
+            case "UAH":
+                Console.WriteLine("Input currency you get(EUR, USD):");
+                switch (Console.ReadLine())
+                {
+                    case "EUR": 
+                        Console.WriteLine("Input value(UAH):");
+                        Console.WriteLine("You get " + Math.Round(Ex.UahToEur(Convert.ToDecimal(Console.ReadLine())), 2) + " EUR"); break;
+                    case "USD": 
+                        Console.WriteLine("Input value(UAH):");
+                        Console.WriteLine("You get " + Math.Round(Ex.UahToUsd(Convert.ToDecimal(Console.ReadLine())), 2) + " USD"); break;
+                    default: break;
+                }
+                break;
+            case "EUR":
+                Console.WriteLine("Input value(EUR):");
+                Console.WriteLine("You get " + Math.Round(Ex.EurToUah(Convert.ToDecimal(Console.ReadLine())), 2) + " UAH"); break;
+            case "USD":
+                 Console.WriteLine("Input value(USD):");
+                Console.WriteLine("You get " + Math.Round(Ex.UsdToUah(Convert.ToDecimal(Console.ReadLine())), 2) + " UAH"); break;
+            default: break;
+        }
+    }
+}
